85eda8fea0f2887260ad0b05dd06eaf906a3abf0
arqprtma <ariqp63@gmail.com>
2024-01-08 23:55:17 +0700

    fix : email verif

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,13 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Auth\Passwords\CanResetPassword as CanResetPasswordTrait;
 
-class User extends Authenticatable implements CanResetPassword
+class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
 {
     use Notifiable, CanResetPasswordTrait;
 
